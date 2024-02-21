266665a43264f5c66aa2cea29fee39a36dcd7f16
Yuri Kovalov <yuri@yurikoval.com>
2024-02-21 12:34:58 +0800

    cp files

--- install.sh
+++ install.sh
@@ -28,5 +28,5 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    cp $dir/$file ~/.$file
 done
