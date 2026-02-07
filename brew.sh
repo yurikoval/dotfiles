5ad2fdc759394109bbd9f5203aa855cc77f2146b
Yuri Kovalov <yuri@yurikoval.com>
2026-02-07 13:03:56 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -13,72 +13,115 @@ brew update
 brew upgrade
 
 # Add taps
+brew tap anomalyco/tap
 brew tap heroku/brew
+brew tap knative-extensions/kn-plugins
 
 # Install binaries
 brew install ack \
   asdf \
   bat \
+  cmake \
+  cocoapods \
+  colima \
+  coreutils \
   cowsay \
   diff-so-fancy \
+  dnsx \
+  docker \
   ffmpeg \
+  flyctl \
   fortune \
   fzf \
-  gpg \
+  gemini-cli \
   gh \
-  git \
+  git-fixup \
   git-lfs \
   heroku \
   jq \
+  k3d \
+  kind \
   lolcat \
+  nmap \
+  anomalyco/tap/opencode \
   openssh \
-  openssl \
+  parallel \
+  peco \
   rename \
   stow \
+  subfinder \
+  subversion \
   terraform \
   tldr \
+  transmission-cli \
   tree \
-  webp \
+  watch \
   wget \
+  wp-cli \
   youtube-dl \
+  yt-dlp \
   z \
   zsh \
-  zsh-autosuggestions
+  zsh-autosuggestions \
+  knative-extensions/kn-plugins/func \
+  knative-extensions/kn-plugins/quickstart
 
 # Install casks
 brew install --cask 1password
+brew install --cask airflow
 brew install --cask alfred
+brew install --cask antigravity
+brew install --cask arc
 brew install --cask bartender
+brew install --cask blackhole-16ch
 brew install --cask brave-browser
+brew install --cask caffeine
+brew install --cask chatgpt
+brew install --cask chromium
+brew install --cask claude
+brew install --cask claude-code
 brew install --cask cyberduck
 brew install --cask discord
 brew install --cask divvy
 brew install --cask dotnet-sdk
 brew install --cask figma
+brew install --cask gcloud-cli
 brew install --cask google-chrome
-brew install --cask google-cloud-sdk
+brew install --cask gpt4all
 brew install --cask hyper
 brew install --cask imageoptim
 brew install --cask iterm2
+brew install --cask jan
+brew install --cask macdown
+brew install --cask menumeters
+brew install --cask mountain-duck
 brew install --cask notion
 brew install --cask obsidian
 brew install --cask omnifocus
-brew install --cask omnigraffle
 brew install --cask omnioutliner
 brew install --cask omniplan
-brew install --cask menumeters
-brew install --cask protonvpn protonmail-bridge proton-drive
+brew install --cask openaudible
+brew install --cask opera
+brew install --cask proton-drive
+brew install --cask proton-mail-bridge
+brew install --cask protonvpn
 brew install --cask quicksilver
-brew install --cask skitch
 brew install --cask signal
+brew install --cask skitch
 brew install --cask slack
+brew install --cask sonos
 brew install --cask spectacle
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask tor-browser
 brew install --cask tower
-brew install --cask visual-studio-code
+brew install --cask transmission
+brew install --cask typora
+brew install --cask vlc
 brew install --cask whatsapp
+brew install --cask xscope
 
-# Install fzf
-brew install fzf
+# Configure fzf key bindings and fuzzy completion
 $(brew --prefix)/opt/fzf/install
 
 # Install oh-my-zsh
